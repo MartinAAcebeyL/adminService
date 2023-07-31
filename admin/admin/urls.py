7424032b921559fa8fc22e12f17c003ac5395ecd
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-07-30 22:46:05 -0400

    fixed urls

--- admin/admin/urls.py
+++ admin/admin/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from apps.products.urls import urlpatterns
+from apps.products.urls import urlpatterns as url_products
+from apps.clients.urls import urlpatterns as url_clients
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(urlpatterns)),
+    path('api/v1/products/', include(url_products)),
+    path('api/v1/clients/', include(url_clients))
 ]
--- admin/apps/products/urls.py
+++ admin/apps/products/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import ProductViewSet
 
 urlpatterns = [
-    path('products', ProductViewSet.as_view({
+    path('', ProductViewSet.as_view({
         'get': 'list',
         'post': 'create',
     }), name='products'),
-    path('products/<str:pk>', ProductViewSet.as_view({
+    path('<str:pk>', ProductViewSet.as_view({
         'get': 'retrieve',
         'put': 'update',
         'delete': 'destroy',
