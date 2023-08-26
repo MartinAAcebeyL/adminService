d62d4b2d48f5cca410f8712933c647da62c1974a
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-08-26 00:08:06 -0400

    resolve mistake about DJANGO_SETTINGS_MODULE

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "antares.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
